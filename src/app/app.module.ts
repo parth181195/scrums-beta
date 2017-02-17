86c732609acc525c5dc6457077255ff741a69f6f
parth181195 <parthjansari@outlook.com>
2017-02-17 20:21:11 +0530

    initial comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import 'hammerjs';
+import 'hammer-timejs';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    SignupPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+
+// declare var Hammer;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public pan: number = 0;
   constructor(public navCtrl: NavController) {
     
   }
+    panEvent(e) {
+      console.log(e);
+      
+    this.pan++
+  }
+  //   ngOnInit(){
+  // }
+  }
 
-}
